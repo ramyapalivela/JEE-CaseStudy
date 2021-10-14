cccdbc80d88608198e56a397e161e71083d7c838
ramyapalivela <67458594+ramyapalivela@users.noreply.github.com>
2021-10-14 19:56:27 +0530

    Case study Assignment

--- Blood-Donation-Camp/src/main/java/com/example/demo/repo/DonationCampRepository.java
+++ Blood-Donation-Camp/src/main/java/com/example/demo/repo/DonationCampRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repo;
 import java.time.LocalDate;
 import java.util.List;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -16,10 +16,10 @@ import com.example.demo.entity.DonationCampDetails;
 @Repository
 public interface DonationCampRepository extends JpaRepository<DonationCampDetails, Integer> {
 	public List<DonationCampDetails> findAll();
-	public List<DonationCampDetails> findByLocation(String location);
+	public List<DonationCampDetails> findByDonationCampLocation(String location);
 	public int removeCampDetails(int number);
-	@Query(nativeQuery = true, value = "update donationCamp set donationCampName=:name,donationCampLocation =: location,donationCampDate=:date,"
-			+ "donationCampPhoneNumber =: phonenumber,donationCampPincode=:pin where campNumber=:number")
+	@Query(nativeQuery = true, value = "update donationCamp set donationCampName=:name,donationCampLocation=:location,donationCampDate=:date,"
+			+ "donationCampPhoneNumber=:phonenumber,donationCampPincode=:pin where campNumber=:number")
 	@Modifying
 	@Transactional
 	public int updateCamp(@Param("number") int campNumber,@Param("name") String donationCampName,@Param("location") String donationCampLocation,
