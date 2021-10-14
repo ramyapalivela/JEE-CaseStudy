f4654a1f01d7d99119902d286c2ac23ed92e196b
Ramya <ramyapalivela123@gmail.com>
2021-10-14 14:18:26 +0530

    Case study Assignment

--- Donar-service-mvc/src/main/java/com/example/demo/DonarServiceMvcApplication.java
+++ Donar-service-mvc/src/main/java/com/example/demo/DonarServiceMvcApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class DonarServiceMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DonarServiceMvcApplication.class, args);
+	}
+	@Bean
+	public ModelAndView mdlView() {
+		return new ModelAndView();
+	}
+	@Bean
+	public RestTemplate template() {
+		return new RestTemplate();
 	}
 
 }
